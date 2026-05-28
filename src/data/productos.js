258a169d7d43d0faeadc2a5ebf057737309d0324
consuelojerezv <co.jerezv@duocuc.cl>
2026-05-27 20:15:30 -0400

    initial commit ferreteria dos

---
+++ src/data/productos.js
@@ -0,0 +1,9 @@
+export const productos = [
+  { id: 1, nombre: "Cemento 25kg", precio: 12500 },
+  { id: 2, nombre: "Arena m3", precio: 45000 },
+  { id: 3, nombre: "Grava m3", precio: 42000 },
+  { id: 4, nombre: "Malla ACMA", precio: 58000 },
+  { id: 5, nombre: "Perfil Metalcon", precio: 9500 },
+  { id: 6, nombre: "Yeso cartón", precio: 18000 },
+  { id: 7, nombre: "Plancha Zinc", precio: 28000 }
+];
